2d996a0e961923b0fed04021869c00633bc057d5
panch8 <98569130+panch8@users.noreply.github.com>
2022-12-06 00:42:30 +0100

    first local commit

---
+++ index.js
@@ -0,0 +1,60 @@
+function pressButton(currentKey) {
+  var activeKey = document.querySelector(`.${currentKey}`);
+  activeKey.classList.add("pressed");
+  setTimeout(function () {
+    activeKey.classList.remove("pressed");
+  }, 100);
+}
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      const crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      const kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "s":
+      const snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "d":
+      const tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      const tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "k":
+      const tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "l":
+      const tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+
+    default:
+      console.log(key);
+      break;
+  }
+}
+
+//click event.
+
+const numDrumButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHtml = this.textContent;
+    makeSound(buttonInnerHtml);
+    pressButton(buttonInnerHtml);
+  });
+}
+
+// key event
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+  pressButton(event.key);
+});
